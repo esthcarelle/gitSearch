710c11210f21de607006cb2d56c2d9470e2bf7fa
esthcarelle <esthcarelle@gmail.com>
2019-09-01 20:46:14 +0200

    add getprofile function

--- src/app/gitservice.service.ts
+++ src/app/gitservice.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {  HttpClient } from '@angular/common/http';
 import {Github} from './github'
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -12,13 +13,11 @@ export class GitserviceService {
    }
   
   private user:string="esthcarelle";
-  private clientId:'364fb8f9a5cada030543';
-  private clientpassword:'7eb68359515222df8cf85f76a79eab5de3c21cb3';
-  getProfileInfo(){
-    return  this.http.get('https://api.github.com/users/' + this.user+ '?client_id=' + this.clientId +'&client_secret='+ this.clientpassword );
+    getProfileInfo(){
+    return  this.http.get('https://api.github.com/users/' + this.user+ '?client_id=' + environment.clientId +'&client_secret='+ environment.clientpassword );
    }
    getProfileRepos(){
-    return  this.http.get('https://api.github.com/users/' + this.user+ '/repos?client_id=' + this.clientId +'&client_secret='+ this.clientpassword );
+    return  this.http.get('https://api.github.com/users/' + this.user+ '/repos?client_id=' + environment.clientId +'&client_secret='+ environment.clientpassword );
    
    }
    getName(user:string){
