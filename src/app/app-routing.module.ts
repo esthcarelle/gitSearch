76e42a8c6e10081d4b7c4bec65a270cd1a0d8234
esthcarelle <esthcarelle@gmail.com>
2019-09-01 19:14:19 +0200

    add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ReadMoreComponent } from './read-more/read-more.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent},
+  { path: 'readmore', component: ReadMoreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
