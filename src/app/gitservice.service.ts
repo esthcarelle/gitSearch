16e4a272b371a99e453fc0a3016b24db5aa031f4
esthcarelle <esthcarelle@gmail.com>
2019-09-01 21:51:54 +0200

    spacing  errors

--- src/app/gitservice.service.ts
+++ src/app/gitservice.service.ts
@@ -11,13 +11,15 @@ export class GitserviceService {
   constructor(private http:HttpClient) {
    
    }
-  
+  private  clientId:'364fb8f9a5cada030543';
+  private  clientpassword:'7eb68359515222df8cf85f76a79eab5de3c21cb3'
+ 
   private user:string="esthcarelle";
     getProfileInfo(){
-    return  this.http.get('https://api.github.com/users/' + this.user+ '?client_id=' + environment.clientId +'&client_secret='+ environment.clientpassword );
+    return  this.http.get('https://api.github.com/users/' + this.user+ '?client_id=' + this.clientId +'&client_secret='+ this.clientpassword );
    }
    getProfileRepos(){
-    return  this.http.get('https://api.github.com/users/' + this.user+ '/repos?client_id=' + environment.clientId +'&client_secret='+ environment.clientpassword );
+    return  this.http.get('https://api.github.com/users/' + this.user+ '/repos?client_id=' + this.clientId +'&client_secret='+ this.clientpassword );
    
    }
    getName(user:string){
